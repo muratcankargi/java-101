f443d0e7126de774f7c72c86ac89503a2c4f1a37
muratcankargi <48772741+muratcankargi@users.noreply.github.com>
2022-10-09 17:02:14 +0300

    Update sicakligaGoreEtkinlikOnerme.java

--- sicakligaGoreEtkinlikOnerme.java
+++ sicakligaGoreEtkinlikOnerme.java
@@ -7,9 +7,9 @@ public class sicakligaGoreEtkinlikOnerme {
 
         /*
         Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
-Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
-Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
-Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+        Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+        Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+        Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
          */
         Scanner input = new Scanner(System.in);
         double hava;
