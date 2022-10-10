0a0cd226c45b0cf4eb3d55e66a4800621c92897c
muratcankargi <48772741+muratcankargi@users.noreply.github.com>
2022-10-10 20:42:58 +0300

    Update ustluSayiHesaplama.java

--- ustluSayiHesaplama.java
+++ ustluSayiHesaplama.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
+
 public class ustluSayiHesaplama {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
-        int a,b,islem=1;
+        Scanner input = new Scanner(System.in);
+        int a, b, islem = 1;
         System.out.print("Üst alma değerlerini giriniz (a^b)\na : ");
-        a=input.nextInt();
+        a = input.nextInt();
 
         System.out.print("b : ");
-        b=input.nextInt();
+        b = input.nextInt();
 
-        for(int i =0;i<b;i++){
-            islem*=a;
+        for (int i = 0; i < b; i++) {
+            islem *= a;
         }
-        System.out.println(a+"^"+b+" : "+islem);
+        System.out.println(a + "^" + b + " : " + islem);
     }
 }
