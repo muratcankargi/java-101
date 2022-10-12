8b135f7c28480dff14ec2d9eb1977f8bf7227e08
muratcankargi <48772741+muratcankargi@users.noreply.github.com>
2022-10-12 17:39:37 +0300

    Create ustAlmaRecursive.java

---
+++ ustAlmaRecursive.java
@@ -0,0 +1,23 @@
+package Functions;
+import java.util.*;
+public class ustAlmaRecursive {
+
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        int a,b;
+
+        System.out.print("Taban değeri giriniz : ");
+        a=input.nextInt();
+
+
+        System.out.print("Üs değeri giriniz : ");
+        b=input.nextInt();
+        System.out.println("Sonuç : "+power(a,b));
+    }
+    static int power(int a,int b){
+        if(b==0)
+            return 1;
+        else
+            return a*(power(a,--b));
+    }
+}
