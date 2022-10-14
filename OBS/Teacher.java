5ad0c718d7450ee67d98cda046322980a0954c71
muratcankargi <muratcankargi@hotmail.com>
2022-10-14 14:57:49 +0300

    Teacher.java created

---
+++ OBS/Teacher.java
@@ -0,0 +1,17 @@
+package OBS;
+class Teacher {
+    String name;
+    int mpNo;
+    String branch;
+
+    Teacher(String name, int mpNo, String branch) {
+        this.name = name;
+        this.mpNo = mpNo;
+        this.branch = branch;
+    }
+    void print() {
+        System.out.println("Ad : " + this.name);
+        System.out.println("Telefon :" + this.mpNo);
+        System.out.println("Bölümü : " + this.branch);
+    }
+}
