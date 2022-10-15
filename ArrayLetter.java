9aa9aa1cfb44ba2cae002b539b4b93a7ae6c95aa
muratcankargi <muratcankargi@hotmail.com>
2022-10-15 13:25:00 +0300

    Create ArrayLetter.java

---
+++ ArrayLetter.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class ArrayLetter {
+    public static void main(String[] args) {
+        String[][] matrix = new String[7][4];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if ((i == 0 && j != 3) || (i == matrix.length - 1 && j != 3) || (i == 3 && j != 3)) {
+                    matrix[i][j] = "*  ";
+                } else if ((j == 0) || (j == matrix[i].length - 1 && i != 0 && i != 3 && i != 6)) {
+                    matrix[i][j] = "* ";
+                } else {
+                    matrix[i][j] = "   ";
+                }
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
